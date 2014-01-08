681c1382006aa9c791594e3542d74bda9b018e68
skellz <sherwin.kelly@gmail.com>
2014-01-08 12:01:22 -0500

    Testing rails cast 136

--- store/public/javascripts/application.js
+++ store/public/javascripts/application.js
@@ -1,18 +1,28 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-jQuery.ajaxSetup({ 
+// jQuery.ajaxSetup({ 
+//   'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+// })
+
+// jQuery.fn.submitWithAjax = function() {
+//   this.submit(function() {
+//     $.post(this.action, $(this).serialize(), null, "script");
+//     return false;
+//   })
+//   return this;
+// };
+
+// $(document).ready(function() {
+//   $("#new_review").submitWithAjax();
+// })
+jQuery.ajaxSetup({
   'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
 })
 
-jQuery.fn.submitWithAjax = function() {
-  this.submit(function() {
-    $.post(this.action, $(this).serialize(), null, "script");
+$(document).ready(function(){
+  $("#new_review").submit(function(){
+    $.post($(this).attr("action"), $(this).serialize(), null, "script")
     return false;
   })
-  return this;
-};
-
-$(document).ready(function() {
-  $("#new_review").submitWithAjax();
 })
